9a6422f4b955ef3bb8148017821cc4dd34f1a2ba
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-04 20:34:42 +0545

    form added

--- Day_3/search.php
+++ Day_3/search.php
@@ -1,2 +1,19 @@
-<?php
+<?php 
 
+#echo"Hello World!<br>";
+
+#echo"<pre>";
+#print_r($_GET);
+#echo"</pre>";
+
+echo"my name is ".$_GET['search'];
+
+?>
+
+## Get = $_GET;
+## Post =$_POST;
+
+<a href="/">Go back to home page!!!!</a>
+
+
+## header("Location: index.html")
