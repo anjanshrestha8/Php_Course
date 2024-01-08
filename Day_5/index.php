0bf886d4365a161fcfb529ab4164e81c711e3369
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-08 20:21:34 +0545

    data base added

---
+++ Day_5/index.php
@@ -0,0 +1,18 @@
+#(working with different files and connecting these files in one single files)
+
+<?php
+
+
+#include
+#require
+
+#include_once
+#require_once
+
+include'header.php';
+
+echo"<br>";
+echo"Navbar";
+
+require"footer.php";
+?>
