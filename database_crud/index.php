6472dddf2a184a646c025ffc80c947706b26c5c3
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-09 11:06:43 +0545

    data base connection code added

--- database_crud/index.php
+++ database_crud/index.php
@@ -2,9 +2,36 @@
 
 
 include'function.php';
-echo"Connecting Database";
 
-$result = mysqli_connect("localhost","root","anjanshrestha8","todo");
+// connection establish
+
+echo"<h1>Connecting Database</h1>";
+$connection = mysqli_connect("localhost","root","anjanshrestha8","todo");
+print_this($connection);
+
+// selecting data
+
+echo"<h1>Selecting Database</h1>";
+$result = mysqli_query($connection,"select * from todo_tb");
+print_this($result);
+
+// fetching
+echo"<h1>Fetching Database</h1>";
+$rows = mysqli_fetch_all($result);
+print_this($rows);
+
+// inserting
+
+// echo"<h1>Inserting Database</h1>";
+// $insert = mysqli_query($connection,"insert into todo_tb(ID,NAME,AGE) values(2,"Shrestha",22)");
+// print_this($insert);
+
+
+
+
+
+
+
 
 
 ?>
