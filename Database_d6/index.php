1d11d3d3e228b645fde2a3b160dfc6546277740b
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-09 10:04:15 +0545

    data base connection code added

--- Database_d6/index.php
+++ Database_d6/index.php
@@ -6,23 +6,23 @@ include'function.php';
 # functional approach
 
 // connection to data base
-$conn = mysqli_connect('localhost','root','anjanshrestha8','');
+$conn = mysqli_connect('localhost','root','anjanshrestha8','todo');
 print_it($conn);
 
 
 
 // select data from database
 
-$result = mysqli_query($conn,"select * from ");
-
+$result = mysqli_query($conn,"select * from todo_tb");
+// print_it($result);
 
 // fetch all
 $rows=mysqli_fetch_all($result);
 print_it($rows);
 
-// fectch all with associative
-$rows=mysqli_fetch_all($result);
-print_it($rows,MYSQLI_ASSOC);
+// fetch all with associative
+## $rows=mysqli_fetch_all($result);
+## print_it($rows ,MYSQL_BOTH);
 
 
 
