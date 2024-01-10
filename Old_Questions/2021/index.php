25198733338d7133f6a5bca1f5f6a0deece8bfde
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-10 17:38:56 +0545

    od question solution

---
+++ Old_Questions/2021/index.php
@@ -0,0 +1,24 @@
+<!-- CURD Operation -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CURD</title>
+</head>
+<body>
+    <section>
+        <h1>CURD Operation!!!!!!</h1>
+    </section>
+
+    <section>
+        <form action="" method="post">
+            <input type="text" name="quote" require autocomplete="quote">
+            <button type="submit">Save</button>
+        </form>
+
+    </section>
+    
+</body>
+</html>
