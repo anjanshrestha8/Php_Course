db6aa9ea41df4ff1136870add69f29236a084e04
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-01-15 21:53:14 +0545

    form validationa dded

--- Old_Questions/FROM/script.js
+++ Old_Questions/FROM/script.js
@@ -1,3 +1,27 @@
 function validation(){
-    console.log("js");
+
+    // validation of password
+    let password =document.getElementById("pass");
+    if(password.value.length<8){
+        alert("Please check your password");
+        document.getElementById("pass").value="";
+        return false;
+    }
+    else{
+        alert("Login!!!!!")
+    }
+    // validation of email
+    let email = document.getElementById("mail");
+    let regExp =/^([a-z A-Z 0-9 \._-]+)@([a-z 0-9]+.)([a-z]+.)([a-z]?)$/;
+    if(regExp.test(email)){
+        alert("valid email");
+        return true;
+    }
+    else{
+        alert("Input valid email");
+        // document.getElementById("user").value= " ";
+        return false;
+    }
+    
+
 }
