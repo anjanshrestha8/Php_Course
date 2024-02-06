8743bfacbc5f354e163b61633c0f1bf4447f1574
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-02-06 17:20:18 +0545

    files added

--- Old_Questions/php/delete.php
+++ Old_Questions/php/delete.php
@@ -1,8 +1,8 @@
 <?php
     require_once"config.php";
-    $value = $_POST['data'];
 
-    $query="INSERT INTO php (title) values('$value') ";
+    $id=$_POST['id'];
+    $query="DELETE FROM php WHERE id='$id'";
     $query_run=mysqli_query($conn,$query);
     if(!$query_run){
         die(mysqli_error());
--- Old_Questions/php/index.php
+++ Old_Questions/php/index.php
@@ -1,5 +1,9 @@
 <?php 
     require_once"config.php";
+    $query="SELECT * FROM php";
+    $query_run=mysqli_query($conn,$query);
+
+    $fetch=mysqli_fetch_all($query_run,MYSQLI_ASSOC);
 
 ?>
 
@@ -16,18 +20,30 @@
     <!-- create -->
     <section> 
             <form action="create.php" method="POST">
-                <input type="text"  id="data">
+                <input type="text" name="data">
                 <button type="submit">create</button>
             </form>
     </section>
 
      <!-- read -->
      <section> 
-        <li>a</li>
-        <li>b</li>
-        <li>v</li>
-        <li>b</li>
-        <li>v</li>
+       <?php foreach($fetch as $value):?>
+       <li>
+        <?php echo $value['title'];?>
+        <!-- DELETE -->
+            <form action="delete.php" method="POST">
+                <input type="hidden" name="tar" value="<?php echo$value['id'];?>">
+                <button type="submit">delete</button>
+            </form>
+            <!-- UPDATE -->
+            <form action="update.php" method="POST">
+                <input type="hidden" name="tar">
+
+                <button type="submit">update</button>
+            </form>
+       </li>
+       <?php endforeach?>
+
             
     </section>
     
